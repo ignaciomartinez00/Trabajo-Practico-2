3636fc3758acceca84dc84707823f5bb283f4805
ignaciomartinez00 <63934479+ignaciomartinez00@users.noreply.github.com>
2020-12-11 19:30:04 -0300

    Update Ejercicio 39.c

--- Ejercicio 39.c
+++ Ejercicio 39.c
@@ -32,18 +32,18 @@ typedef struct
     int pf;
 }datos_t2;
 
-struct cola
+struct lista
 {
     datos_t dato;
-    struct cola *l;
+    struct lista *l;
 };
 int  menu(void);
 
 int main()
 {
     int op;
-    datos_t dato[100],auxo;
-    struct cola *p=NULL, *aux,*u;
+    datos_t auxo;
+    struct lista *p=NULL, *aux,*u,*r;
     unsigned int val; //Valor Ascii Letra
     int i=0,j,k;
     ///toma de datos  //tome los datos de medición de potencia, cargados por el usuario, de un sistema de medición a distancia.
@@ -51,53 +51,61 @@ int main()
     {
         switch(menu())
         {
+
         case 1:
-        printf("datos:\n");
+            aux=(struct lista*)malloc(sizeof(struct lista));
+            if(aux)
+            {
+
+                printf("datos:\n");
         printf("ingrese potencia: ");
-        fflush(stdin);
-        scanf("%hhu",&dato[i].potencia);
+        scanf("%hhu",&aux->dato.potencia);
         printf("ingrese descripcion: ");
         fflush(stdin);
-        scanf("%s",(dato[i].desc));
-        val=dato[i].desc[0];
-        printf("\nvalor:%d\t",val);
-        (dato[i].estado)=val*dato[i].potencia;   ///Donde estado es la multiplicación del valor ASCII de la primer letra de la descripción por la potencia.
-        printf("descripcion:%s\t",dato[i].desc);
-        printf("estado:%d\n",dato[i].estado);
-        i++;
-   ///ORDEN mayor a menor
-    for(j=0;j<i-1;j++)
-    {
-        for(k=j+1;k<i;k++)
-        {
-            if(((dato[j].potencia))<((dato[k].potencia)))  //es al reves porque dsp en la lista es LIFO??
-            {
-                auxo=dato[k];
-                dato[k]=dato[j];
-                dato[j]=auxo;
-            }
-        }
-    }
+        scanf("%s",aux->dato.desc);
+       (aux->dato.estado)=atoi(aux->dato.desc)*(aux->dato.potencia);   ///Donde estado es la multiplicación del valor ASCII de la primer letra de la descripción por la potencia.
+        printf("descripcion:%s\t",aux->dato.desc);
+        printf("estado:%d\n",aux->dato.estado);
 
-    ///CARGA EN LISTA  Los datos cargados se deberán guardar en una lista ordenada, de mayor a menor, del valor de potencia.
-    printf("\nRevision\n");
-    for(j=0;j<i;j++)
-{
-                aux=(struct cola*)malloc(sizeof(struct cola));
-                if(aux)///sin abreviar if(aux!=0)
+
+
+
+                if(p==NULL)
                 {
-                    aux->dato=dato[j];
-                    if(p==NULL) p=u=aux;
-                    else
-                    {
-                        u->l=aux;
-                        u=aux;
-                    }
+                    p=u=aux;
                     u->l=NULL;
-                }
-                else printf("\n Memoria insuficiente\n");
-}
 
+                }
+                else
+                {
+                    r=p;
+                    while(1)
+                    {
+                        if(strcmp(r->dato.potencia , aux->dato.potencia)==1)  ///si el dato del puntero r es mayor que el dato del puntero aux
+                        {
+
+                            aux->l=p;
+                            p=aux;
+                            break;
+                        }
+
+                        ///No es el primer lugar entonces busco en el resto 30:00
+                        while(r->l!=0)  ///mientras el lazo del puntero no llegue al final
+                        {
+                            if(strcmp(r->dato.potencia , aux->dato.potencia)==-1)   ///si el dato del siguiente elemento del puntero r  es menor al dato del puntero aux
+                            {
+                                r=r->l;  ///r es igual al siguiente
+                            }
+                            else break;  ///si el dato del siguiente elemento del puntero r es mayor  se rompe el mientras
+                        }
+                        aux->l=r->l;
+                        r->l=aux;
+                        break;
+                    }
+
+                }
+            }
+i=0;
     ///Una vez finalizada la carga guardar los datos en un archivo de organización directa llamado "potencia.dat" imprimiendo en pantalla los datos.
     FILE*fp;
     datos_t2 bf2;
@@ -106,20 +114,27 @@ int main()
         printf("no se puede crear/abrir archivo");
         return 0;
     }
-    for(j=0;j<i;j++)
-    {
-       if(p)
-                {
+
+
+           aux=p;
+           fread(&bf2,sizeof(datos_t2),1,fp);
+            while(aux)
+            {
+
                     bf2.potencia=p->dato.potencia;
                     strcpy(bf2.desc,p->dato.desc);
                     bf2.estado=p->dato.estado;
                     bf2.pf=j;
+                    bf2.pf=i;
+                    i++;
                     fwrite(&bf2,sizeof(datos_t2),1,fp);
                     aux=p;
                     p=p->l;
                     free(aux);
-                }
-    }
+                    aux=aux->l;
+                    fread(&bf2,sizeof(datos_t2),1,fp);
+            }
+            system("pause");
     fclose(fp);
     break;
 
@@ -160,4 +175,3 @@ int menu(void)
     scanf("%d",&op);
     return op;
 }
-
